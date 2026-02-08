063e22d9826e5f7f6f789c36b09603eefbb5744e
fatguylilcoat98 <stangman9898@gmail.com>
2026-02-08 06:00:31 -0800

    Fix Blocked Request error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: true
   },
   server: {
-    port: 3000,
-  },
+    host: true
+  }
 })
