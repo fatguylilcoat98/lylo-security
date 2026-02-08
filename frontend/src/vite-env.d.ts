aa6878c0a681b3fd447db939537e409ab1ae6629
fatguylilcoat98 <stangman9898@gmail.com>
2026-02-08 11:51:18 -0800

    Create vite-env.d.ts

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_API_KEY: string
+  readonly VITE_TAVILY_API_KEY: string
+  readonly VITE_STRIPE_PUBLISHABLE_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
