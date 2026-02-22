d966f88a7886be795a4eaf6af864bd7e452d7099
fatguylilcoat98 <stangman9898@gmail.com>
2026-02-22 03:29:20 -0800

    Delete backend/intelligence_data directory

--- backend/intelligence_data/__init__.py
+++
@@ -1,6 +0,0 @@
-# The "Bridge" File
-# This exposes your new Nuclear Data to the main application.
-
-from .personas import PERSONA_DEFINITIONS, PERSONA_EXTENDED, PERSONA_TIERS
-from .vibes import VIBE_LABELS, VIBE_STYLES
-from .hooks import get_random_hook, get_all_hooks
